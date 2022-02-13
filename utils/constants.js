a796a573d190f8059afdedfdfba74b2b49e1163a
veralea <veralea@rambler.ru>
2022-02-13 22:35:19 +0200

    feat: connecting to a remote database

--- utils/constants.js
+++ utils/constants.js
@@ -3,7 +3,9 @@ const NOT_FOUND_ERROR_CODE = 404;
 const VALID_ERROR_CODE = 400;
 const CREATED_CODE = 201;
 const SUCCESS_CODE = 200;
-const MONGO_SERVER = 'mongodb://localhost:27017/newsdb';
+// const MONGO_SERVER = 'mongodb://localhost:27017/newsdb';
+const MONGO_SERVER =
+'mongodb://veralea:O4GFBh7JmyLBq6saYIA2jdFz8afKlxzGUHHTaeiX1hukJ3taZ24A0u9FYt1rANKJ89bTpHbnsUwb1YHgGWcp5w==@veralea.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@veralea@';
 
 module.exports = {
   INTERNAL_SERVER_ERROR_CODE,
